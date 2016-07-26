320b17ffff727f636b72b4b03bbfd19e84a266c7
blackbicbiro <blackbicbiro@gmail.com>
2016-07-26 17:39:23 +0100

    Installer for slide show Pi

---
+++ slideshowinstall.sh
@@ -0,0 +1,15 @@
+
+
+#https://github.com/bpatrik/PiGallery
+
+apt-get install apache2
+apt-get install mysql-server
+apt-get install php5 libapache2-mod-php5 php-gd-y
+sudo apt-get install php5-gd php5-mysql
+service apache2 restart
+
+cd /var/www/html
+wget 
+
+
+#sql password: raspberry
